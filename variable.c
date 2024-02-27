df289c4737cf5c8e8a68493c68bcedfe77a276c7
Rohithstrike <rohithb@Rohiths-Laptop.local>
2024-02-27 20:24:47 +0530

    About Variables

---
+++ variable.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+
+    int   a = 5;    // a and b is a container which stores the data 
+    float b = 5.5;
+
+    printf("%d\n",a);
+    printf("%f\n",b);   // %f is formate specifier
+
+    return 0;
+
+}
