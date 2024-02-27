25df82eebde285e69a18db58bdd234b3946e3925
Rohithstrike <rohithb@Rohiths-Laptop.local>
2024-02-27 20:32:37 +0530

    Types of Variables

--- variable.c
+++ variable.c
@@ -1,11 +1,16 @@
 #include<stdio.h>
 int main(){
 
-    int   a = 5;    // a and b is a container which stores the data 
-    float b = 5.5;
+    int    a = 5;    // a and b is a container which stores the data 
+    float  b = 5.5;
+    double d = 5.5555555; 
+    char   c = 'a';
+     
 
     printf("%d\n",a);
     printf("%f\n",b);   // %f is formate specifier
+    printf("%lf\n",d);
+    printf("%c\n",c);
 
     return 0;
 
